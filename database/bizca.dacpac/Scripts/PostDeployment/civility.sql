2cbe8633ba8c23b72f5b7df664e1a53c5509427a
Francis Tsafack <Francis.Tsafack_ext@s-money.fr>
2020-11-23 15:31:51 +0100

    [feature] make dacpac cross platform

--- database/bizca.dacpac/Scripts/PostDeployment/civility.sql
+++ database/bizca.dacpac/Scripts/PostDeployment/civility.sql
@@ -1,28 +1,24 @@
-﻿	declare	@civility table (
-		[civilityId]	smallint	not null,
-		[civilityCode]	varchar(5)	not null
+﻿merge into [ref].[civility] as target
+	using 
+	(
+		values   (1,'Mr')
+				,(2,'Mrs')
+				,(3,'Miss')
+				,(4,'Other')
+	) as source(civilityId, civilityCode) on target.civilityId = source.civilityId
+when matched then 
+	update
+		set civilityCode = source.civilityCode,
+			lastUpdate  = getutcdate()
+when not matched by target then
+	insert
+	(
+		civilityId, 
+		civilityCode
 	)
-
-	insert into @civility 
-	values   (1,'Mr')
-			,(2,'Mrs')
-			,(3,'Miss')
-			,(4,'Other')
-
-	merge into [ref].[civility] as target
-		using @civility source on target.civilityId = source.civilityId
-	when matched then 
-		update
-			set civilityCode = source.civilityCode,
-			    lastUpdate  = getutcdate()
-	when not matched by target then
-		insert
-		(
-			civilityId, 
-			civilityCode
-		)
-		values
-		(
-			source.civilityId, 
-			source.civilityCode
-		);
+	values
+	(
+		source.civilityId, 
+		source.civilityCode
+	);
+go
