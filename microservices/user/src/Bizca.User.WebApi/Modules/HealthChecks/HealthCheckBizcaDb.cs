4309e2757894cb2ce5d5b3792155bda618618e2f
Francis Tsafack <Francis.Tsafack_ext@s-money.fr>
2021-02-15 00:35:20 +0100

    [feature] add health check

---
+++ microservices/user/src/Bizca.User.WebApi/Modules/HealthChecks/HealthCheckBizcaDb.cs
@@ -0,0 +1,22 @@
+﻿namespace Bizca.User.WebApi.Modules.HealthChecks
+{
+    using Bizca.Core.Api.HealthChecks;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+
+    /// <summary>
+    ///     Bizca database health check.
+    /// </summary>
+    public class HealthCheckBizcaDb : HealthCheckDatabase
+    {
+        /// <summary>
+        ///     Creates an instance of <see cref="HealthCheckBizcaDb"/>
+        /// </summary>
+        /// <param name="environment">host environment</param>
+        /// <param name="configuration">configuration.</param>
+        public HealthCheckBizcaDb(IWebHostEnvironment environment, IConfiguration configuration)
+            : base(environment, configuration.GetValue<string>("BizcaDatabase:ConnectionString"))
+        {
+        }
+    }
+}
