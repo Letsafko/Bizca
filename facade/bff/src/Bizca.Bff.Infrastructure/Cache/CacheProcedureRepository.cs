9ecc510e26e28ba2535080b7b9f38e844373e0d4
Francis Tsafack <Francis.Tsafack_ext@s-money.fr>
2021-06-18 00:48:02 +0200

    [feature] add caching for bundle and procedure

---
+++ facade/bff/src/Bizca.Bff.Infrastructure/Cache/CacheProcedureRepository.cs
@@ -0,0 +1,39 @@
+﻿namespace Bizca.Bff.Infrastructure.Cache
+{
+    using Bizca.Bff.Domain.Referentials.Procedure;
+    using Bizca.Core.Domain.Cache;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public sealed class CacheProcedureRepository : IProcedureRepository
+    {
+        private readonly IProcedureRepository decorated;
+        private readonly ICacheProvider cache;
+        public CacheProcedureRepository(ICacheProvider cache, IProcedureRepository decorated)
+        {
+            this.decorated = decorated;
+            this.cache = cache;
+        }
+
+        public async Task<Procedure> GetProcedureByTypeIdAndCodeInseeAsync(int procedureTypeId, string codeInsee)
+        {
+            string cacheKey = GetCacheKey($"{codeInsee}_{procedureTypeId}");
+            return await cache.GetOrCreateAsync
+            (
+                cacheKey, 
+                () => decorated.GetProcedureByTypeIdAndCodeInseeAsync(procedureTypeId, codeInsee)
+            );
+        }
+
+        public async Task<IEnumerable<Procedure>> GetProceduresAsync()
+        {
+            string cacheKey = GetCacheKey("all");
+            return await cache.GetOrCreateAsync(cacheKey, () => decorated.GetProceduresAsync());
+        }
+
+        private string GetCacheKey(object value)
+        {
+            return $"{nameof(Procedure).ToLower()}_{value}";
+        }
+    }
+}
