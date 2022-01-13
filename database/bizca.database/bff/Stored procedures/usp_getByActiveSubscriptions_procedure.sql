d32c2a7d75eacaaba4e9efc04ca7997fc40ff6b5
Francis TSAFACK <wilfrancis2001@yahoo.fr>
2022-01-13 08:32:21 +0100

    [database] retrieve procedures for actives subscriptions

---
+++ database/bizca.database/bff/Stored procedures/usp_getByActiveSubscriptions_procedure.sql
@@ -0,0 +1,36 @@
+﻿create procedure [bff].[usp_getByActiveSubscriptions_procedure]
+as
+begin
+
+	;with activeProcedures 
+	as
+	(
+		select 
+			 s.procedureTypeId, 
+			 s.organismId
+		from bff.subscription s
+		join bff.[procedure] p 
+		on 
+		(	
+			p.procedureTypeId = s.procedureTypeId and
+			p.organismId = s.organismId
+		)
+		where subscriptionStatusId = 2
+		group by s.procedureTypeId, s.organismId
+		having count(s.procedureTypeId) > 0
+	)
+
+	select 
+		p.procedureTypeId,
+		p.procedureHref,
+		p.organismId,
+		p.settings
+	from [bff].[procedure] p
+	join activeProcedures ap 
+	on
+	(
+		ap.organismId = p.organismId and
+		ap.procedureTypeId = p.procedureTypeId
+	)
+
+end
